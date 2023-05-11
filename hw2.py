7777dc6a5665c5da364156ebc55d0a7b55bd3142
蔡 炘晏 <71438514+colenator123-design@users.noreply.github.com>
2023-05-12 02:39:47 +0800

    Create hw2.py

---
+++ hw2.py
@@ -0,0 +1,14 @@
+#1
+for i in range(1, 11, 2):
+    print(i)
+#2
+total = 0
+for i in range(2, 21, 2):
+    total += i
+print(total)
+#3
+for i in range(10, 51, 5):
+    print(i)
+#4
+my_list = [i for i in range(10)]
+print(my_list)
